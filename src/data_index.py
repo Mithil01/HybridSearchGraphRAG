b4440332707b3f449602d385a2fb9162a3fd87f1
Mithil Gaonkar <mithilgaonkar01@gmail.com>
2024-11-22 15:46:11 -0500

    Update data_index.py

--- src/data_index.py
+++ src/data_index.py
@@ -9,18 +9,18 @@ import os
 
 st.write(
     "Has environment variables been set:",
-    os.environ["openai_api_key"] == st.secrets["OPENAI_API_KEY"],
+    os.environ["OPENAI_API_KEY"] == st.secrets["OPENAI_API_KEY"],
     os.environ['NEO4J_USERNAME'] == st.secrets["NEO4J_USERNAME"],
     os.environ['NEO4J_URI'] == st.secrets["NEO4J_URI"],
     os.environ['neo4j_pass'] == st.secrets["NEO4J_PASSWORD"]
 )
 
-openai_api_key = os.environ['OPENAI_API_KEY'] 
-client = OpenAI(api_key=openai_api_key)
+OPENAI_API_KEY = os.environ['OPENAI_API_KEY'] 
+client = OpenAI(api_key=OPENAI_API_KEY)
 NEO4J_URI = os.environ['NEO4J_URI']
 NEO4J_USERNAME = os.environ['NEO4J_USERNAME']
 NEO4J_PASSWORD = os.environ['NEO4J_PASSWORD']
-neo4j_pass = os.environ['neo4j_pass']
+#neo4j_pass = os.environ['neo4j_pass']
 
 system_prompt = """
 Given some initial query, generate synonyms or related keywords up to 10 in total, considering possible cases of pluralization, common expressions, etc.
